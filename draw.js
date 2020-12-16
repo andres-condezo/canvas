845b2183231d99db711fbb6abfd3ca669822a01c
adrs <ra.condezomonge@gmail.com>
2020-12-15 21:08:03 -0500

    init canvas.

--- draw.js
+++ draw.js
@@ -1,1 +1,17 @@
-alert("Sachita es magia");
+var d = document.getElementById("draw");
+var lienzo = d.getContext("2d");
+console.log(lienzo);
+
+lienzo.beginPath();
+lienzo.strokeStyle = "blue";
+lienzo.moveTo(50,10);
+lienzo.lineTo(250, 2);
+lienzo.stroke();
+lienzo.closePath();
+
+lienzo.beginPath();
+lienzo.strokeStyle = "red";
+lienzo.moveTo(180,10);
+lienzo.lineTo(290, 200);
+lienzo.stroke();
+lienzo.closePath();
