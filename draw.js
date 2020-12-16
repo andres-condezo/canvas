1dba35ae4a9d1dac4843a2e6a408852f84f31ada
adrs <ra.condezomonge@gmail.com>
2020-12-15 21:53:56 -0500

    Add function draw.

--- draw.js
+++ draw.js
@@ -1,17 +1,15 @@
 var d = document.getElementById("draw");
 var lienzo = d.getContext("2d");
-console.log(lienzo);
 
+dibujarLinea("pink", 10, 300, 220, 10);
+dibujarLinea("red", 300, 10, 10, 220);
+
+function dibujarLinea(color, xin, yin, xfi, yfi)
+{
 lienzo.beginPath();
-lienzo.strokeStyle = "blue";
-lienzo.moveTo(50,10);
-lienzo.lineTo(250, 2);
+lienzo.strokeStyle = color;
+lienzo.moveTo(xin, yin);
+lienzo.lineTo(xfi, yfi);
 lienzo.stroke();
 lienzo.closePath();
-
-lienzo.beginPath();
-lienzo.strokeStyle = "red";
-lienzo.moveTo(180,10);
-lienzo.lineTo(290, 200);
-lienzo.stroke();
-lienzo.closePath();
+}
