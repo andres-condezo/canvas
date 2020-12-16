6d5d0dbc86d6d4e52b43920deddf307b06f46d12
adrs <ra.condezomonge@gmail.com>
2020-12-16 11:39:51 -0500

    for loop and bttn.

--- draw.js
+++ draw.js
@@ -3,22 +3,14 @@ var lienzo = d.getContext("2d");
 var lineas = 30;
 var l = 0;
 var yi, xf;
-var color2 ="#FAA";
+var color2 = "#FAA";
 
-for(l = 0; l < lineas; l++)
-{
-  yi = 10 * l;
-  xf = 10 * (l + 1);
-  dibujarLinea(color2, 0, yi, xf, 300);
-  console.log("Linea " +l)
-}
-
-for(l = 0; l < lineas; l++)
-{
+for (l = 0; l < lineas; l++) {
   yi = 10 * l;
   xf = 10 * (l + 1);
   dibujarLinea(color2, yi, 0, 300, xf);
-  console.log("Linea " +l)
+  dibujarLinea(color2, 0, yi, xf, 300);
+  console.log("Linea " + l);
 }
 
 dibujarLinea(color2, 1, 1, 299, 1);
@@ -26,12 +18,12 @@ dibujarLinea(color2, 299, 1, 299, 299);
 dibujarLinea(color2, 1, 1, 1, 299);
 dibujarLinea(color2, 1, 299, 299, 299);
 
-function dibujarLinea(color, xin, yin, xfi, yfi)
-{
-lienzo.beginPath();
-lienzo.strokeStyle = color;
-lienzo.moveTo(xin, yin);
-lienzo.lineTo(xfi, yfi);
-lienzo.stroke();
-lienzo.closePath();
+function dibujarLinea(color, xin, yin, xfi, yfi) {
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xin, yin);
+  lienzo.lineTo(xfi, yfi);
+  lienzo.stroke();
+  lienzo.closePath();
 }
+
