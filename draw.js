9fc84cb7e67f7be89cb3b0b50ecee12c69d2a7e7
adrs <ra.condezomonge@gmail.com>
2020-12-16 13:41:38 -0500

    Add new function .

--- draw.js
+++ draw.js
@@ -4,24 +4,8 @@ var boton = document.getElementById("bttn_enviar");
 boton.addEventListener("click", drawperclick);
 
 var d = document.getElementById("draw");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0;
-var yi, xf;
-var color2 = "#FAA";
-
-for (l = 0; l < lineas; l++) {
-  yi = 10 * l;
-  xf = 10 * (l + 1);
-  dibujarLinea(color2, yi, 0, 300, xf);
-  dibujarLinea(color2, 0, yi, xf, 300);
-  console.log("Linea " + l);
-}
-
-dibujarLinea(color2, 1, 1, 299, 1);
-dibujarLinea(color2, 299, 1, 299, 299);
-dibujarLinea(color2, 1, 1, 1, 299);
-dibujarLinea(color2, 1, 299, 299, 299);
 
 function dibujarLinea(color, xin, yin, xfi, yfi) {
   lienzo.beginPath();
@@ -33,6 +17,24 @@ function dibujarLinea(color, xin, yin, xfi, yfi) {
 }
 
 function drawperclick() {
-  alert("No me toques ahi" + text.value);
-}
+  var lineas = parseInt(text.value);
+  var l = 0;
+  var yi, xf;
+  var color2 = "#FAA";
+  var espacio = ancho / lineas;
 
+  d.width = d.width;
+
+  for (l = 0; l < lineas; l++) {
+    yi = espacio * l;
+    xf = espacio * (l + 1);
+    dibujarLinea(color2, yi, 0, 300, xf);
+    dibujarLinea(color2, 0, yi, xf, 300);
+    console.log("Linea " + l);
+  }
+
+  dibujarLinea(color2, 1, 1, 299, 1);
+  dibujarLinea(color2, 299, 1, 299, 299);
+  dibujarLinea(color2, 1, 1, 1, 299);
+  dibujarLinea(color2, 1, 299, 299, 299);
+}
